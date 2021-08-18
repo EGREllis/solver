cd19ec7e5e33c94153bf78025494f7712a413a2d
Edward Ellis <edwardellis.egre@gmail.com>
2021-08-18 15:18:55 +0100

    Implemented a backtracking sudoku solver

--- src/main/java/net/ellise/sudoku/App.java
+++ src/main/java/net/ellise/sudoku/App.java
@@ -3,6 +3,7 @@ package net.ellise.sudoku;
 import net.ellise.sudoku.io.ArrayPuzzleReader;
 import net.ellise.sudoku.io.MapPuzzleReader;
 import net.ellise.sudoku.model.ArrayPuzzleImpl;
+import net.ellise.sudoku.model.BacktrackingSolver;
 import net.ellise.sudoku.model.Puzzle;
 import net.ellise.sudoku.view.cli.CliView;
 
@@ -18,5 +19,14 @@ public class App
         System.out.println();
         Puzzle arrayPuzzle = new ArrayPuzzleReader("puzzle/1.dat").call();
         System.out.println(new CliView().render(arrayPuzzle));
+        System.out.println();
+        BacktrackingSolver solver = new BacktrackingSolver();
+        if (solver.solve(arrayPuzzle)) {
+            System.out.println("Has been solved:");
+            System.out.println(new CliView().render(arrayPuzzle));
+        } else {
+            System.out.println("Has not been solved:");
+            System.out.println(new CliView().render(arrayPuzzle));
+        }
     }
 }
---
+++ src/main/java/net/ellise/sudoku/model/BacktrackingSolver.java
@@ -0,0 +1,69 @@
+package net.ellise.sudoku.model;
+
+public class BacktrackingSolver {
+
+    public boolean solve(Puzzle puzzle) {
+        Place startPlace = findNextBlank(puzzle);
+        for (int newValue : Puzzle.DIGITS) {
+            if (solve(puzzle, startPlace, newValue)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean solve(Puzzle puzzle, Place place, int value) {
+        puzzle.setCellAt(place, value);
+        if (isValid(puzzle, place, value)) {
+            Place nextPlace = findNextBlank(puzzle);
+            if (nextPlace == null) {
+                return true;
+            }
+            for (int newValue : Puzzle.DIGITS) {
+                if (solve(puzzle, nextPlace, newValue)) {
+                    return true;
+                }
+            }
+        }
+        puzzle.setCellAt(place, 0);
+        return false;
+    }
+
+    private boolean isValid(Puzzle puzzle, Place place, int value) {
+        // Valid row
+        for (int row : Puzzle.HEIGHT) {
+            if (row != place.getY() && puzzle.getCellAt(new Place(place.getX(), row)) == value) {
+                return false;
+            }
+        }
+        // Valid column
+        for (int col : Puzzle.WIDTH) {
+            if (col != place.getX() && puzzle.getCellAt(new Place(col, place.getY())) == value) {
+                return false;
+            }
+        }
+        // Valid square
+        int squareX = (place.getX()-1) / 3;
+        int squareY = (place.getY()-1) / 3;
+        for (int sy = squareY * 3 + 1; sy < squareY * 3 + 4; sy++) {
+            for (int sx = squareX * 3 + 1; sx < squareX * 3 + 4; sx++) {
+                if ( (place.getX() != sx || place.getY() != sy) /* False only on the spot */
+                    && puzzle.getCellAt(new Place(sx, sy)) == value) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private Place findNextBlank(Puzzle puzzle) {
+        Place result = null;
+        for (Place place : Puzzle.AREA) {
+            if (puzzle.getCellAt(place) == 0) {
+                result = place;
+                break;
+            }
+        }
+        return result;
+    }
+}
